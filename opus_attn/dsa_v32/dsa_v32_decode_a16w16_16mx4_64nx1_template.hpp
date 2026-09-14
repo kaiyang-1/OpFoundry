4d6a9f0805f9421cdc5fd11714f09cb0cc951bc5
kaiyang-1 <Kai.Yang1@amd.com>
2026-09-14 05:16:52 -0500

    softmax with register pin

--- opus_attn/dsa_v32/dsa_v32_decode_a16w16_16mx4_64nx1_template.hpp
+++ opus_attn/dsa_v32/dsa_v32_decode_a16w16_16mx4_64nx1_template.hpp
@@ -2,13 +2,14 @@
 
 #include <opus/opus.hpp>
 #include "defs.h"
+#include <bit>
 
 using opus::operator""_I;
 
 namespace dsa_v32_decode_a16w16_16mx4_64nx1 {
 
 constexpr int Q_AGPR_BASE = 0;
-constexpr int S_VGPR_BASE = 0;
+constexpr int S_VGPR_BASE = 128;
 
 template<class T> constexpr int Q_ELEMS          = T::Q_TILE_SIZE * T::D_QK_SIZE / T::WARP_SIZE;
 template<class T> constexpr int Q_LOAD_INSTS     = Q_ELEMS<T> / T::VEC_Q;
@@ -20,11 +21,14 @@ template<class T> constexpr int K_AGPR_BASE      = Q_AGPR_BASE + Q_LOAD_INSTS<T>
 
 template<class T> constexpr int S_ELEMS          = T::GEMM0_E_N * T::W_M * T::W_N / T::WARP_SIZE;
 template<class T> constexpr int S_VGPR_PER_TILE  = T::W_M * T::W_N / T::WARP_SIZE;
+template<class T> constexpr int P_VGPR_BASE      = S_VGPR_BASE;
 
 template<class T> constexpr int O_ELEMS          = T::Q_TILE_SIZE * T::D_VO_SIZE / T::WARP_SIZE;
+constexpr int O_VGPR_BASE = 0;
 
 template<class T> using v_q_t = opus::array<opus::vector_t<typename T::D_ATTN, T::VEC_Q>, Q_LOAD_INSTS<T>>;
 template<class T> using v_s_t = opus::vector_t<typename T::D_ACC, S_ELEMS<T>>;
+template<class T> using v_p_t = opus::vector_t<typename T::D_ATTN, S_ELEMS<T>>;
 template<class T> using v_o_t = opus::vector_t<typename T::D_ACC, O_ELEMS<T>>;
 
 template<class T, int I> constexpr int kv_sub_tile_off = I * T::NUM_WARPS * T::smem_d_rpt * T::smem_brick;
@@ -209,12 +213,118 @@ __device__ inline void compute_qk(char* smem_kv, const U& u_rk, const VQ& v_q, V
     });
 }
 
-template<class Traits, class VS>
-__device__ typename Traits::D_ACC attn_row_max(const VS& v_s);
-template<class Traits, class VS>
-__device__ typename Traits::D_ACC attn_row_sum(const VS& v_s);
-template<class Traits, class VS>
-__device__ void attn_mask_oob_kv_tile(VS& v_s, int valid_kv_len, int kv_tile_idx, opus::u32_t neg_inf_v);
+template<int THR_X, int THR_Y>
+__device__ inline void attn_mask_vec2_imm(opus::u32_t rel, opus::u32_t neg_inf_v,
+                                          opus::u32_t& x_ref, opus::u32_t& y_ref) {
+    opus::u64_t x_mask, y_mask;
+    asm volatile(
+        "v_cmp_lt_i32_e64 %0, %6, %7\n\t"
+        "v_cmp_lt_i32_e64 %1, %6, %9\n\t"
+        "v_cndmask_b32_e64 %2, %4, %8, %0\n\t"
+        "v_cndmask_b32_e64 %3, %5, %8, %1\n\t"
+        : "=s"(x_mask), "=s"(y_mask), "=v"(x_ref), "=v"(y_ref)
+        : "v"(x_ref), "v"(y_ref), "v"(rel),
+          "n"(THR_X), "v"(neg_inf_v), "n"(THR_Y)
+        : "vcc"
+    );
+}
+
+template<class T, class V>
+__device__ inline void attn_mask_oob_kv_tile(V& v_s, int valid_kv_len, int kv_tile_idx, opus::u32_t neg_inf_v) {
+    using D_ACC = typename T::D_ACC;
+    using D_ACC_X2 = opus::vector_t<D_ACC, 2>;
+    using U32_X2 = opus::vector_t<opus::u32_t, 2>;
+
+    constexpr int c_pack = T::W_M * T::W_N / T::WARP_SIZE;
+
+    const int last_valid_kv_pos = valid_kv_len - 1;
+    const int k_start_pos = kv_tile_idx * T::KV_TILE_SIZE;
+    int lane_id = opus::thread_id_x() % T::WARP_SIZE;
+    asm volatile("" : "+v"(lane_id));
+    const int lane_group = lane_id / T::W_N;
+
+    opus::static_for<T::GEMM0_E_N>([&](auto i_n) {
+        const int k_pos = k_start_pos + i_n.value * T::W_N + lane_group * c_pack;
+        const opus::u32_t rel = static_cast<opus::u32_t>(last_valid_kv_pos - k_pos);
+
+        opus::static_for<c_pack / 2>([&](auto i_pair) {
+            constexpr int idx = i_n.value * c_pack + i_pair.value * 2;
+            auto pair_bits = __builtin_bit_cast(U32_X2, opus::slice(v_s, opus::number<idx>{}, opus::number<idx + 2>{}));
+            opus::u32_t x_ref = pair_bits[0];
+            opus::u32_t y_ref = pair_bits[1];
+            attn_mask_vec2_imm<i_pair.value * 2, i_pair.value * 2 + 1>(rel, neg_inf_v, x_ref, y_ref);
+            pair_bits[0] = x_ref;
+            pair_bits[1] = y_ref;
+            opus::set_slice(v_s, __builtin_bit_cast(D_ACC_X2, pair_bits), opus::number<idx>{}, opus::number<idx + 2>{});
+        });
+    });
+}
+
+template<class T, class V>
+__device__ inline typename T::D_ACC attn_row_max(const V& v_s) {
+    using D_ACC = typename T::D_ACC;
+    constexpr opus::index_t s_len = opus::vector_traits<V>::size();
+    D_ACC row_max = opus::numeric_limits<D_ACC>::lowest();
+    opus::static_for<s_len>([&](auto i) { row_max = max(row_max, v_s[i.value]); });
+
+    opus::vector_t<opus::u32_t, 2> res32 = __builtin_amdgcn_permlane32_swap(std::bit_cast<opus::u32_t>(row_max), std::bit_cast<opus::u32_t>(row_max), false, true);
+    row_max = max(std::bit_cast<float>(res32.x), std::bit_cast<float>(res32.y));
+    opus::vector_t<opus::u32_t, 2> res16 = __builtin_amdgcn_permlane16_swap(std::bit_cast<opus::u32_t>(row_max), std::bit_cast<opus::u32_t>(row_max), false, true);
+    return max(std::bit_cast<float>(res16.x), std::bit_cast<float>(res16.y));
+}
+
+template<class T, class V>
+__device__ inline typename T::D_ACC attn_row_sum(const V& v_s) {
+    using D_ACC = typename T::D_ACC;
+    constexpr opus::index_t s_len = opus::vector_traits<V>::size();
+    D_ACC row_sum = 0.0f;
+    opus::static_for<s_len>([&](auto i) { row_sum += v_s[i.value]; });
+
+    opus::vector_t<opus::u32_t, 2> res32 = __builtin_amdgcn_permlane32_swap(std::bit_cast<opus::u32_t>(row_sum), std::bit_cast<opus::u32_t>(row_sum), false, true);
+    row_sum = std::bit_cast<float>(res32.x) + std::bit_cast<float>(res32.y);
+    opus::vector_t<opus::u32_t, 2> res16 = __builtin_amdgcn_permlane16_swap(std::bit_cast<opus::u32_t>(row_sum), std::bit_cast<opus::u32_t>(row_sum), false, true);
+    return std::bit_cast<float>(res16.x) + std::bit_cast<float>(res16.y);
+}
+
+template<class T, class VO>
+__device__ inline void scale_o_tile(VO& v_o, typename T::D_ACC scale) {
+    using namespace opus;
+    using acc2_t = vector_t<typename T::D_ACC, 2>;
+    auto o_pairs = reinterpret_cast<acc2_t*>(&v_o);
+    static_for<O_ELEMS<T> / 2>([&](auto i) {
+        [[clang::amdgpu_pin_vgpr(O_VGPR_BASE + i.value * 2)]]
+        o_pairs[i.value] = o_pairs[i.value] * scale;
+    });
+}
+
+template<class T, class VS, class VP, class VO>
+__device__ inline void softmax_tile(VS& v_s, VP& v_p, VO& v_o,
+                                    typename T::D_ACC& m_row, typename T::D_ACC& l_row,
+                                    typename T::D_ACC temperature_scale) {
+    using namespace opus;
+    using D_ACC = typename T::D_ACC;
+    using attn2_t = vector_t<typename T::D_ATTN, 2>;
+
+    const D_ACC row_max = max(m_row, attn_row_max<T>(v_s) * temperature_scale);
+    const D_ACC rescale = __builtin_amdgcn_exp2f(m_row - row_max);
+
+    m_row = row_max;
+
+    static_for<S_ELEMS<T>>([&](auto i) {
+        v_s[i.value] = __builtin_amdgcn_exp2f(__builtin_fmaf(v_s[i.value], temperature_scale, -row_max));
+    });
+
+    l_row = __builtin_fmaf(l_row, rescale, attn_row_sum<T>(v_s));
+
+    auto p_pairs = reinterpret_cast<attn2_t*>(&v_p);
+    static_for<S_ELEMS<T> / 2>([&](auto i) {
+        auto pair = slice(v_s, number<i.value * 2>{}, number<i.value * 2 + 2>{});
+        [[clang::amdgpu_pin_vgpr(P_VGPR_BASE<T> + i.value)]]
+        p_pairs[i.value] = cast<typename T::D_ATTN>(pair);
+    });
+
+    scale_o_tile<T>(v_o, rescale);
+}
 
 template<class Traits>
 __device__ void attention_tiles(const dsa_v32_a16w16_kargs& kargs,
@@ -228,6 +338,7 @@ __device__ void attention_tiles(const dsa_v32_a16w16_kargs& kargs,
     using namespace opus;
     using T = remove_cvref_t<Traits>;
     using D_ATTN = typename T::D_ATTN;
+    using D_ACC  = typename T::D_ACC;
 
     int lane_id = thread_id_x() % T::WARP_SIZE;
     asm volatile("" : "+v"(lane_id));
@@ -243,6 +354,8 @@ __device__ void attention_tiles(const dsa_v32_a16w16_kargs& kargs,
     auto u_skv        = make_layout_skv<T>(warp_id);
     auto u_rk         = make_layout_rk<T>(lane_id);
 
+    const u32_t neg_inf_v = std::bit_cast<u32_t>(-numeric_limits<D_ACC>::infinity());
+
     for (int tile_idx = tile_begin; tile_idx < tile_end; ++tile_idx) {
         auto kv_pages = load<1>(g_kv_indices, u_kv_indices, tile_idx * T::KV_TILE_SIZE);
 
@@ -254,6 +367,13 @@ __device__ void attention_tiles(const dsa_v32_a16w16_kargs& kargs,
         v_s_t<T> v_s;
         clear(v_s);
         compute_qk<T>(smem_kv, u_rk, v_q, v_s);
+
+        if ((tile_idx + 1) * T::KV_TILE_SIZE > valid_kv_len) {
+            attn_mask_oob_kv_tile<T>(v_s, valid_kv_len, tile_idx, neg_inf_v);
+        }
+
+        v_p_t<T> v_p;
+        softmax_tile<T>(v_s, v_p, v_o, m_row, l_row, temperature_scale);
 
     }
 }
@@ -290,7 +410,7 @@ __device__ void decode_one_req(const dsa_v32_a16w16_kargs& kargs, int batch_idx,
                        smem_kv, v_q, v_o, m_row, l_row, temperature_scale);
 
     const D_ACC o_scale = (l_row > D_ACC(0.0f)) ? (D_ACC(1.0f) / l_row) : D_ACC(0.0f);
-    static_for<O_ELEMS<T>>([&](auto i) { v_o[i.value] *= o_scale; });
+    scale_o_tile<T>(v_o, o_scale);
 
     int lane_id_o = thread_id_x() % T::WARP_SIZE;
     asm volatile("" : "+v"(lane_id_o));
